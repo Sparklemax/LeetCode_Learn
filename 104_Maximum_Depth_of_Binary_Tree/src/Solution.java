ae0a4f75bc044e38b86e7c48cd75369f945b1913
Sparklemax <sparklemax@outlook.com>
2019-08-06 19:55:54 +0800

    第104题 添加思路与复杂度分析

--- 104_Maximum_Depth_of_Binary_Tree/src/Solution.java
+++ 104_Maximum_Depth_of_Binary_Tree/src/Solution.java
@@ -3,6 +3,13 @@
  * @date 2019/8/6 19:18
  */
 
+/*
+* 思路：递归
+* 复杂度分析：
+* 时间复杂度：每个结点只访问一次，因此时间复杂度为 O(N)，
+* 空间复杂度：：在最糟糕的情况下，树是完全不平衡的，递归将会被调用 NN 次（树的高度）
+* */
+
 class Solution {
     public int maxDepth(TreeNode root) {
         if(root == null)
