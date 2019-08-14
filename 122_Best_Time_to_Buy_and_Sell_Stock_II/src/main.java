5a0e70ef6c620c7d258eecd893d2798b208b6185
Sparklemax <sparklemax@outlook.com>
2019-08-14 20:14:12 +0800

    第122题

---
+++ 122_Best_Time_to_Buy_and_Sell_Stock_II/src/main.java
@@ -0,0 +1,12 @@
+/**
+ * @author Sparklemax
+ * @date 2019/8/13 21:06
+ */
+
+public class main {
+    public static void main(String[] args) {
+        Solution sl = new Solution();
+        System.out.println(sl.maxProfit(new int[]{7, 1, 5, 3, 6, 4}));
+        System.out.println(sl.maxProfit(new int[]{7, 6, 4, 3, 1}));
+    }
+}
