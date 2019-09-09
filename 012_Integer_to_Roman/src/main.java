6cecb2a25538954a297e20dbc7179cde4ce0168b
Sparklemax <sparklemax@outlook.com>
2019-09-09 20:47:30 +0800

    第12题

---
+++ 012_Integer_to_Roman/src/main.java
@@ -0,0 +1,16 @@
+/**
+ * @author Sparklemax
+ * @date 2019/9/7 15:48
+ */
+
+public class main {
+    public static void main(String[] args) {
+        Solution sl = new Solution();
+        System.out.println(sl.intToRoman(3));
+        System.out.println(sl.intToRoman(4));
+        System.out.println(sl.intToRoman(9));
+        System.out.println(sl.intToRoman(58));
+        System.out.println(sl.intToRoman(1994));
+
+    }
+}
